5c54453a0bd499f96f68242e71faef396a136174
olesiavelychko <velychko@work.co>
2017-06-08 14:50:50 -0400

    Initial remote_state module

---
+++ interface.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  default     = "us-east-1"
+  description = "The AWS region."
+}
+
+variable "prefix" {
+  default     = "olesia_examplecom"
+  description = "The name of our org, i.e. examplecom."
+}
+
+variable "environment" {
+  default     = "development"
+  description = "The name of our environment, i.e. development."
+}
+
+# This will return the ID of the bucket we're creating.
+
+output "s3_bucket_id" {
+  value = "${aws_s3_bucket.remote_state.id}"
+}
